2b98fb6d1c03bf44f091c28df2143315c40ac7fd
lordpvi <lord.pvi@gmail.com>
2016-03-21 17:40:28 +0300

    1

---
+++ SmartCA/SmartCA.Infrastructure/DomainBase/EntityBase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmartCA.Infrastructure.DomainBase
+{
+    public abstract class EntityBase
+    {
+        protected EntityBase():this(null)
+        {
+        }
+
+        protected EntityBase(object key)
+        {
+            this.key = key;
+        }
+
+        private object key;
+        public object Key {
+            get { return key; }
+        }
+
+        public override bool Equals(object entity)
+        {
+            if (entity == null || !(entity is EntityBase)) return false;
+            return this == (EntityBase)entity;
+        }
+
+        public static bool operator ==(EntityBase base1, EntityBase base2)
+        {
+            if ((object)base1 == null && (object)base2 == null)
+                return true;
+
+            if ((object)base1 == null || (object)base2 == null)
+                return false;
+
+            if (base1.key != base2.key) return false;
+
+            return true;
+        }
+
+        public static bool operator !=(EntityBase base1, EntityBase base2)
+        {
+            return !(base1 == base2);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.key.GetHashCode();
+        }
+    }
+}
